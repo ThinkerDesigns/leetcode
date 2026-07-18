ddb6782de6bcc0c3e687db48aa3630d004e9a0fe
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-07-18 13:21:14 -0700

    Add binary search implementation in 704.py
    Implement binary search algorithm to find target in sorted list.

---
+++ 704.py
@@ -0,0 +1,16 @@
+# Binary Search
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            middle = (left+right) // 2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] > target:
+                right = middle - 1
+                middle = (left+right) // 2
+            elif nums[middle] < target:
+                left = middle + 1
+                middle = (left+right) // 2
+        return -1
