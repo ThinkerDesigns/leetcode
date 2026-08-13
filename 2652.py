a4f968993469926f5b9426d020ee9912c3e58c2c
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-08-13 12:33:42 -0700

    Create 2652.py

---
+++ 2652.py
@@ -0,0 +1,9 @@
+# Sum Multiples
+class Solution:
+    def sumOfMultiples(self, n: int) -> int:
+        result = 0
+        for x in range(1,n+1):
+            if (x % 3 == 0) or (x % 5 == 0) or (x % 7 == 0):
+                result += x
+                print(result)
+        return result
