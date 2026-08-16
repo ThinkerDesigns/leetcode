dcf431636bfcf5e2b7f9d2bf033e4b952b585aad
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-08-16 01:49:34 -0700

    Create 136.py

---
+++ 136.py
@@ -0,0 +1,9 @@
+# Single Number
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        freq = {}
+        for x in nums:
+            freq[x] = freq.get(x,0) + 1
+        for x in freq:
+            if freq[x] == 1:
+                return x
