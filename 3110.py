bb5e7130e829c0d57dd173545e2175ce4fb1e033
Dhruv Bhadauriya <62044304+ThinkerDesigns@users.noreply.github.com>
2026-09-07 00:27:41 -0700

    Implement scoreOfString method in Solution class

---
+++ 3110.py
@@ -0,0 +1,11 @@
+# Score of a String
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        left = 0
+        right = 1
+        result = 0
+        while right < len(s):
+            result = result + (abs(ord(s[left])-ord(s[right])))
+            left += 1
+            right += 1
+        return result
